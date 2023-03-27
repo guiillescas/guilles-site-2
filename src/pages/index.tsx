b613c477181b0bfe268f1c99f04049ff263effcf
Guilherme Illescas <gui.illescas@icloud.com>
2023-03-27 08:14:00 -0300

    feat: finished v1 of journey

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import { JobProps } from 'interfaces/pages/home'
 import { motion } from 'framer-motion'
-import { format } from 'date-fns'
+import { format, formatDistance } from 'date-fns'
 
 import { AppLayout } from 'layouts/AppLayout'
 
@@ -80,6 +80,15 @@ export default function Home() {
           <div className="line two" />
 
           <h1>My journey</h1>
+          <p>
+            See my complete professional journey. You can also, see my{' '}
+            <a
+              href="https://www.linkedin.com/in/guilherme-illescas/"
+              target="_blank"
+            >
+              Linkedin here
+            </a>
+          </p>
 
           <div>
             <nav>
@@ -110,13 +119,86 @@ export default function Home() {
                 animate="show"
                 initial="hide"
               >
-                <h3>{selectedJob.company}</h3>
+                <div className="company-header-wrapper">
+                  <Image
+                    src={selectedJob.companyLogoUrl}
+                    alt={`${selectedJob.company}'s logo`}
+                    width={40}
+                    height={40}
+                  />
+
+                  <div>
+                    <h3>{selectedJob.company}</h3>
+                    <p>
+                      {selectedJob.employmentType} |{' '}
+                      {selectedJob.endedAt
+                        ? formatDistance(
+                            new Date(selectedJob.endedAt),
+                            new Date(selectedJob.startedAt)
+                          )
+                        : format(new Date(selectedJob.startedAt), 'LLL uu')}
+                    </p>
+                  </div>
+                </div>
+
+                {!selectedJob.positions && (
+                  <div className="job-without-positions-info-wrapper">
+                    <h4>{selectedJob.title}</h4>
+
+                    <span>
+                      {format(new Date(selectedJob.startedAt), 'LLL uu')}
+                      {' - '}
+                      {selectedJob.endedAt
+                        ? format(new Date(selectedJob.endedAt), 'LLL uu')
+                        : 'present'}
+                      {' - '}
+                      {formatDistance(
+                        new Date(selectedJob.endedAt || new Date()),
+                        new Date(selectedJob.startedAt)
+                      )}
+                    </span>
+                  </div>
+                )}
+
                 {selectedJob.description && (
                   <div
+                    className="job-description"
                     dangerouslySetInnerHTML={{
                       __html: selectedJob.description
                     }}
                   />
+                )}
+
+                {selectedJob.positions && (
+                  <div className="job-positions-wrapper">
+                    {selectedJob.positions.map(position => (
+                      <div>
+                        <div className="timeline-wrapper"></div>
+
+                        <h4>{position.title}</h4>
+
+                        <span>
+                          {format(new Date(position.startedAt), 'LLL uu')}
+                          {' - '}
+                          {position.endedAt
+                            ? format(new Date(position.endedAt), 'LLL uu')
+                            : 'present'}
+                          {' - '}
+                          {formatDistance(
+                            new Date(position.endedAt || new Date()),
+                            new Date(position.startedAt)
+                          )}
+                        </span>
+
+                        <div
+                          className="job-position-content"
+                          dangerouslySetInnerHTML={{
+                            __html: position.description
+                          }}
+                        />
+                      </div>
+                    ))}
+                  </div>
                 )}
               </motion.div>
             </div>
@@ -136,8 +218,8 @@ export default function Home() {
 
             <p>
               I'm passionate to make this world better with things that I do and
-              with who I am. I love help people to increase themselves and boots
-              them to get results.
+              with who I am. I love helping people to increase themselves and
+              boost them to get results.
             </p>
           </div>
 
